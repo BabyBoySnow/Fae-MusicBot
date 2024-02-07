f5722b26a7525d121f71ad06542413556cc63226
Snow <135516526+BabyBoySnow@users.noreply.github.com>
2024-02-06 20:31:19 -0600

    why not play ping pong?

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2001,7 +2001,26 @@ class MusicBot(discord.Client):
             e.colour = discord.Colour.yellow()
         elif round(self.latency * 1000, 2) > 40:
             e.colour = discord.Colour.red()
-        return Response(e, delete_after=15)
+        return Response(e, delete_after=30)
+
+    async def cmd_pong(self) -> CommandResponse:
+        """
+        Usage:
+            {command_prefix}ping
+
+        Displays the latency between the bot and discord. Relative to expected response time.
+        """
+        e = self._gen_embed()
+        e.title = "Pong"
+        latency = round(self.latency * 1000, 2)
+        e.description = f"\U0001f3d3 {latency}"
+        if 0 <= round(self.latency * 1000, 2) < 25:
+            e.colour = discord.Colour.green()
+        elif 25 <= round(self.latency * 1000, 2) < 40:
+            e.colour = discord.Colour.yellow()
+        elif round(self.latency * 1000, 2) > 40:
+            e.colour = discord.Colour.red()
+        return Response(e, delete_after=30)
 
     async def cmd_help(
         self,
