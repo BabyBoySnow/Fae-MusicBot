15b7c042150b3675844ba30d26973fd0e63ec9bf
Snow <135516526+BabyBoySnow@users.noreply.github.com>
2024-03-04 07:17:01 -0600

    Update bot.py

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2790,7 +2790,7 @@ class MusicBot(discord.Client):
         e = self._gen_embed()
         e.title = "Pong"
         latency = round(self.latency * 1000, 2)
-        e.description = f"\U0001f3d3  {latency}"
+        e.description = f"\U0001f3d3  {latency}ms"
         if 0 <= round(self.latency * 1000, 2) < 25:
             e.colour = discord.Colour.green()
         elif 25 <= round(self.latency * 1000, 2) < 40:
@@ -2809,7 +2809,7 @@ class MusicBot(discord.Client):
         e = self._gen_embed()
         e.title = "Ping"
         latency = round(self.latency * 1000, 2)
-        e.description = f"\U0001f3d3  {latency}"
+        e.description = f"\U0001f3d3  {latency}ms"
         if 0 <= round(self.latency * 1000, 2) < 25:
             e.colour = discord.Colour.green()
         elif 25 <= round(self.latency * 1000, 2) < 40:
